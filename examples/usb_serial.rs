b8bf930e9a7cb77a7cd4c0bd1f38b9062cfc6195
Andrei Litvin <andy314@gmail.com>
2022-05-29 10:39:40 -0400

    USB serial example works

---
+++ examples/usb_serial.rs
@@ -0,0 +1,93 @@
+#![no_std]
+#![no_main]
+
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+use stm32f4xx_hal as hal;
+
+use usb_serial::hal::{pac, prelude::*};
+
+use cortex_m_rt::entry;
+
+use stm32f4xx_hal::otg_fs::{UsbBus, USB};
+use usb_device::prelude::*;
+static mut EP_MEMORY: [u32; 1024] = [0; 1024];
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+
+    rprintln!("Testing USB functionality!");
+
+    let dp = pac::Peripherals::take().unwrap();
+
+    let rcc = dp.RCC.constrain();
+
+    let clocks = rcc
+        .cfgr
+        .use_hse(25.MHz())
+        .sysclk(48.MHz())
+        .require_pll48clk()
+        .freeze();
+
+    let gpioa = dp.GPIOA.split();
+
+    let usb = USB {
+        usb_global: dp.OTG_FS_GLOBAL,
+        usb_device: dp.OTG_FS_DEVICE,
+        usb_pwrclk: dp.OTG_FS_PWRCLK,
+        pin_dm: gpioa.pa11.into_alternate(),
+        pin_dp: gpioa.pa12.into_alternate(),
+        hclk: clocks.hclk(),
+    };
+
+    let usb_bus = UsbBus::new(usb, unsafe { &mut EP_MEMORY });
+
+    let mut serial = usbd_serial::SerialPort::new(&usb_bus);
+
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Andy314")
+        .product("A usb test")
+        .serial_number("TEST123321")
+        .device_class(usbd_serial::USB_CLASS_CDC)
+        .build();
+
+    loop {
+        if !usb_dev.poll(&mut [&mut serial]) {
+            continue;
+        }
+
+        let mut buf = [0u8; 64];
+
+        match serial.read(&mut buf) {
+            Ok(count) if count > 0 => {
+               match core::str::from_utf8(&buf[0..count]) {
+                 Err(e) => {
+                   rprintln!("String is not a valid UTF8: {}", e);
+                 }
+                 Ok(s) => {
+                   rprintln!("Received: {}", s);
+                 }
+               }
+
+                // Echo back in upper case
+                for c in buf[0..count].iter_mut() {
+                    if 0x61 <= *c && *c <= 0x7a {
+                        *c &= !0x20;
+                    }
+                }
+
+                let mut write_offset = 0;
+                while write_offset < count {
+                    match serial.write(&buf[write_offset..count]) {
+                        Ok(len) if len > 0 => {
+                            write_offset += len;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+}
